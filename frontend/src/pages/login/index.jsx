d02caf87b12d1a452906acf1ee7c6a2f2fdd076b
Bach Dao <ngocbach12a1@gmail.com>
2025-01-30 12:37:34 +0700

    Design Login Form

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,8 +1,60 @@
+import { Button, Divider, Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
+import './login.scss'
+
 const LoginPage = () => {
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
     return (
-        <>
-            login page
-        </>
+        <div className='login-page'>
+            <main className='main'>
+                <div className='container'>
+                    <section className='wrapper'>
+                        <div className='heading'>
+                            <h2 className='text text-large'>Đăng nhập</h2>
+                            <Divider />
+                        </div>
+                        <Form
+                            name="basic"
+                            onFinish={onFinish}
+                            autoComplete="off"
+                        >
+                            <Form.Item
+                                labelCol={{ span: 24 }}
+                                label="Email"
+                                name="email"
+                                rules={[{ required: true, message: 'Email không được để trống!' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item
+                                labelCol={{ span: 24 }}
+                                label="Mật khẩu"
+                                name="password"
+                                rules={[{ required: true, message: 'Mật khẩu không được để trống!' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" loading={false}>
+                                    Đăng nhập
+                                </Button>
+                            </Form.Item>
+                            <Divider>Or</Divider>
+                            <p className="text text-normal">Chưa có tài khoản ?
+                                <span>
+                                    <Link to='/register' > Đăng ký </Link>
+                                </span>
+                            </p>
+                        </Form>
+                    </section>
+                </div>
+            </main>
+        </div>
     )
 }
 
