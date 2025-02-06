4f60c3335b5769255bbe9b1aa73e9e4fc18d2454
Bach Dao <ngocbach12a1@gmail.com>
2025-02-07 00:57:47 +0700

    Read File Excel

--- frontend/src/components/Admin/User/data/UserModalImport.jsx
+++ frontend/src/components/Admin/User/data/UserModalImport.jsx
@@ -1,11 +1,15 @@
 import { Modal, Table } from "antd";
 import { InboxOutlined } from '@ant-design/icons';
 import { message, Upload } from 'antd';
+import { useState } from 'react';
+import * as XLSX from 'xlsx';
 
 const { Dragger } = Upload;
 
 const UserModalImport = (props) => {
     const { setIsOpenModalImport, isOpenModalImport } = props
+
+    const [dataExcel, setDataExcel] = useState([]);
 
     const dummyRequest = ({ file, onSuccess }) => {
         setTimeout(() => {
@@ -20,6 +24,8 @@ const UserModalImport = (props) => {
         // limit 1 file uploaded
         maxCount: 1,
         accept: ".csv,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+
+        //prevent upload to server (default action)
         customRequest: dummyRequest,
         onChange(info) {
             const { status } = info.file;
@@ -27,6 +33,26 @@ const UserModalImport = (props) => {
                 console.log(info.file, info.fileList);
             }
             if (status === "done") {
+                if (info.fileList && info.fileList.length > 0) {
+                    const file = info.fileList[0].originFileObj;
+                    let reader = new FileReader();
+                    reader.readAsArrayBuffer(file);
+
+                    reader.onload = (e) => {
+                        let data = new Uint8Array(e.target.result);
+                        let workbook = XLSX.read(data, { type: 'array' });
+                        // // find the name of your sheet in the workbook first
+                        let sheetName = workbook.Sheets[workbook.SheetNames[0]];
+
+                        const json = XLSX.utils.sheet_to_json(sheetName, {
+                            header: ["fullName", "email", "phone"],
+                            range: 1 //skip header row
+                        });
+                        if (json && json.length > 0) {
+                            setDataExcel(json);
+                        }
+                    }
+                }
                 message.success(`${info.file.name} file uploaded successfully.`);
             } else if (status === "error") {
                 message.error(`${info.file.name} file upload failed.`);
@@ -74,7 +100,7 @@ const UserModalImport = (props) => {
                     </p>
                     <p className="ant-upload-text">Click or drag file to this area to upload</p>
                     <p className="ant-upload-hint">
-                        Support for a single or bulk upload. Only accept .csv .xls .xlsx
+                        Support for a single. Only accept .csv .xls .xlsx
                     </p>
                 </Dragger>
 
@@ -82,6 +108,7 @@ const UserModalImport = (props) => {
                     <Table
                         title={() => <span>Dữ liệu upload:</span>}
                         columns={columns}
+                        dataSource={dataExcel}
                     />
                 </div>
             </Modal>
