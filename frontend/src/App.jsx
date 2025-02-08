ec1ed35dd29907b47169526c8fa50119fdbe8412
Bach Dao <ngocbach12a1@gmail.com>
2025-02-08 14:31:26 +0700

    Module Books

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,6 +21,7 @@ import ProtectedRoute from './components/ProtectedRoute';
 import LayoutAdmin from './components/Admin/LayoutAdmin';
 import ManageUser from './pages/admin/user';
 import './styles/reset.scss'
+import BookTable from './components/Admin/Book/BookTable';
 
 
 const Layout = () => {
@@ -92,7 +93,10 @@ export default function App() {
                 },
                 {
                     path: "book",
-                    element: <BookPage />
+                    element:
+                        <ProtectedRoute>
+                            <BookTable />
+                        </ProtectedRoute>
                 },
             ],
         },
