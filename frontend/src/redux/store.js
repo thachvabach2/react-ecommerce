2425c01268fd078985174ff3b9336670aa55f0f1
Bach Dao <ngocbach12a1@gmail.com>
2025-02-18 17:27:16 +0700

    Setup Redux Persist

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,43 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../redux/counter/counterSlice';
 import userReducer from './account/accountSlice';
 
-export const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-        account: userReducer,
-    },
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+    blacklist: ['account'],
+}
+
+const rootReducer = combineReducers({
+    counter: counterReducer,
+    account: userReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
+
+const persistor = persistStore(store)
+
+export { store, persistor }
